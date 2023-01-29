2f9d4051aa17addc93c4388382d8a40f90e72e05
cantevenread <husambunni2016@gmail.com>
2023-01-28 21:49:38 -0500

    Update

--- src/fields/dandelion.rs
+++ src/fields/dandelion.rs
@@ -1,7 +1,19 @@
 use enigo::*;
+use std::{thread, time};
+
 pub fn start_dandelion_gather() {
     let mut enigo = Enigo::new();
 
-    enigo.mouse_move_to(800, 200);
+    enigo.mouse_move_to(785, 840);
+    thread::sleep(time::Duration::from_millis(1000));
+    enigo.mouse_click(MouseButton::Left);
+    thread::sleep(time::Duration::from_millis(1000));
+    enigo.mouse_move_to(785, 775);
+    thread::sleep(time::Duration::from_millis(1000));
+    enigo.mouse_click(MouseButton::Left);
+    thread::sleep(time::Duration::from_millis(1000));
+    enigo.key_sequence_parse(" hello nigga");
+    thread::sleep(time::Duration::from_millis(1000));
+    enigo.key_click(Key::Return);
     println!("Test")
 }
